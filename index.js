7792fadaeda3ba51786298668150670f1d333de9
uvyne-rop <ropchiri7@gmail.com>
2024-04-19 17:31:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -25,24 +25,38 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+// 2. Declare the names of the animals and assign them to variables bessie, dolly, babe, and little.
+const { cow: bessie, sheep: dolly, pig: babe, chicken: little } = { cow: 'Bessie', horse: 'Dolly', sheep: 'Babe', pig: 'Little' };
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+// 3. Declare the colors of the animals and assign them to variables blackAndWhite, black, and pink.
+const { cow: blackAndWhite, sheep: black, pig: pink } = { cow: 'Bessie', sheep: 'Dolly', pig: 'Babe' };
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v] = colors;
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+const [, , , , , indg, ] = colors;
 
-// Objects
+// Objects 
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const { muppetName, color, song, job, partner } = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+const { nestedJob, nestedPartner } = nestedMuppet;
+
+
+
